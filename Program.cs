a31a6d32f3a6e4f6a293f60efd16f2909d8b56b6
DIT_07_226 <DIT_07_226@DIT-07-226-03>
2026-02-27 14:32:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Gazizov.AirplaneProject;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        PassengerAirplane Boeing = new PassengerAirplane("Boeing 737-800", 160, 700, "Авиакомпания", 60, true);
+        Boeing.TakeOff();
+        Boeing.Land();
+        Boeing.PerformPreFlightCheck();
+        Boeing.ShowInfo();
+        Boeing.CheckEngine();
+        Boeing.UpdateMaintenanceLog();
+
+        Airplane Boe = new Airplane("22", 22, 22, "fgdgg");
+        Boe.CheckEngine();
+        Boe.UpdateMaintenanceLog();
+    }
+}
