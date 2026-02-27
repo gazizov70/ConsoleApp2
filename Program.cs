9025a7d54ac3f862c3688cfc785fa9574b499012
DIT_07_226 <DIT_07_226@DIT-07-226-03>
2026-02-27 14:39:33 +0300

    zfdsgfds

--- Program.cs
+++ Program.cs
@@ -5,16 +5,20 @@ class Program
 {
     static void Main()
     {
-        PassengerAirplane Boeing = new PassengerAirplane("Boeing 737-800", 160, 700, "Авиакомпания", 60, true);
-        Boeing.TakeOff();
-        Boeing.Land();
-        Boeing.PerformPreFlightCheck();
-        Boeing.ShowInfo();
-        Boeing.CheckEngine();
-        Boeing.UpdateMaintenanceLog();
+        Airplane a = new Airplane("22", 22, 22, "fgdgg");
+        a.TakeOff();
+        a.Land();
+        a.PerformPreFlightCheck();
+        a.ShowInfo();
+        a.CheckEngine();
+        a.UpdateMaintenanceLog();
 
-        Airplane Boe = new Airplane("22", 22, 22, "fgdgg");
-        Boe.CheckEngine();
-        Boe.UpdateMaintenanceLog();
+        Airplane b = new PassengerAirplane("Boeing 737-800", 160, 700, "Авиакомпания", 60, true);
+        b.TakeOff();
+        b.Land();
+        b.PerformPreFlightCheck();
+        b.ShowInfo();
+        b.CheckEngine();
+        b.UpdateMaintenanceLog();
     }
 }
